79326f689381b277e56a335f14b45fe8ad9373f8
Maximo <maximo@verisian.com>
2026-07-23 18:02:43 +0100

    github bots, test script, helper scripts, and streamlit

--- streamlit/src/constants.py
+++ streamlit/src/constants.py
@@ -1,3 +1,5 @@
-RULES_DIR = "../rules"
+SDTM_RULES_DIR = "../rules"
+ADAM_RULES_DIR = "../adam_rules"
 CG_CSV_PATH = "../conformance-data/CORE-RULES_GitHub Transfer Excel Tracker(SDTM and SDTMIG v2).csv"
 FDA_CSV_PATH = "../conformance-data/CORE-RULES_GitHub Transfer Excel Tracker(FDA Business Rules).csv"
+ADAM_CSV_PATH = "../conformance-data/ADaM Rules Tracker.csv"
